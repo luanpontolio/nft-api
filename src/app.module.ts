fee7ece63996f61d7214db9cbb32b8aeb17e8434
Luan Silveira Pontolio <luan.pontolio@gmail.com>
2022-07-28 22:54:37 -0300

    Update: Add graphQl and alchemy-sdk

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({}),
+  ],
 })
 export class AppModule {}
