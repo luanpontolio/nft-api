adb0ced38199b7f0180d17065750592796b48800
Luan Silveira Pontolio <luan.pontolio@gmail.com>
2022-07-30 00:04:48 -0300

    Update: Include Nftmodule to App module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { NftModule } from './nfts/nfts.module';
+
 
 @Module({
   imports: [
-    GraphQLModule.forRoot({}),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      debug: true,
+      playground: true,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+    NftModule
   ],
 })
 export class AppModule {}
